1801efbb098e3f59fcb3838f4818fc08df55f3ed
Steve Wang <stevew77@gmail.com>
2024-10-14 22:19:04 +0700

    Fixes (#8)
    * fix: sentry settings
    
    * chore: fix docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import sentry from "@sentry/astro";
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 
-process.env = { ...process.env, ...loadEnv(process.env.NODE_ENV || "production", process.cwd(), "") };
+process.env = {
+  ...process.env,
+  ...loadEnv(process.env.NODE_ENV || "production", process.cwd(), ""),
+};
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,10 +27,16 @@ export default defineConfig({
     sitemap(),
     preact({ compat: true, devtools: true }),
     sentry({
+      bundleSizeOptimizations: {
+        excludeReplayIframe: true,
+        excludeReplayShadowDom: true,
+        excludeReplayWorker: true,
+      },
       dsn: process.env.SENTRY_DSN,
       enabled: !!process.env.SENTRY_TOKEN && !!process.env.SENTRY_DSN,
       environment: process.env.NODE_ENV,
       release: process.env.SOURCE_COMMIT || "default",
+      serverInitPath: "./sentry.server.config.js",
       sourceMapsUploadOptions: {
         authToken: process.env.SENTRY_TOKEN,
         project: process.env.SENTRY_PROJECT,
