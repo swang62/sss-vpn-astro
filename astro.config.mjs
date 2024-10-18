c06b45475c2fa606a2342b934fef07c74fa2dd6c
Steve Wang <stevew77@gmail.com>
2024-10-18 21:49:05 +0800

    wip: working form

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sentry from "@sentry/astro";
 import compressor from "astro-compressor";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
+import simpleStackForm from "simple-stack-form";
 import { loadEnv } from "vite";
 
 process.env = {
@@ -25,43 +26,36 @@ export default defineConfig({
     domains: ["picsum.photos"],
   },
   // ORDER MATTERS
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true,
-    }),
-    sentry({
-      bundleSizeOptimizations: {
-        excludeReplayIframe: true,
-        excludeReplayShadowDom: true,
-        excludeReplayWorker: true,
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+    nesting: true,
+  }), sentry({
+    bundleSizeOptimizations: {
+      excludeReplayIframe: true,
+      excludeReplayShadowDom: true,
+      excludeReplayWorker: true,
+    },
+    dsn: process.env.SENTRY_DSN,
+    enabled: !!process.env.SENTRY_TOKEN && !!process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    release: process.env.SOURCE_COMMIT || "default",
+    serverInitPath: "./sentry.server.config.js",
+    sourceMapsUploadOptions: {
+      authToken: process.env.SENTRY_TOKEN,
+      project: process.env.SENTRY_PROJECT,
+    },
+  }), robotsTxt({
+    policy: [
+      {
+        allow: "/",
+        disallow: ["/api", "/error"],
+        userAgent: "*",
       },
-      dsn: process.env.SENTRY_DSN,
-      enabled: !!process.env.SENTRY_TOKEN && !!process.env.SENTRY_DSN,
-      environment: process.env.NODE_ENV,
-      release: process.env.SOURCE_COMMIT || "default",
-      serverInitPath: "./sentry.server.config.js",
-      sourceMapsUploadOptions: {
-        authToken: process.env.SENTRY_TOKEN,
-        project: process.env.SENTRY_PROJECT,
-      },
-    }),
-    robotsTxt({
-      policy: [
-        {
-          allow: "/",
-          disallow: ["/api", "/error"],
-          userAgent: "*",
-        },
-      ],
-    }),
-    sitemap(),
-    compressor({
-      brotli: true,
-      gzip: false,
-    }),
-  ],
+    ],
+  }), sitemap(), simpleStackForm(), compressor({
+    brotli: true,
+    gzip: false,
+  })],
   output: "server",
   server: {
     port: 4321,
