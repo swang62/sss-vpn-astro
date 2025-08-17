e91b198f8ad3fc56223ebb50534f448e65428a7e
Steve Wang <stevew77@gmail.com>
2025-08-18 03:35:45 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -77,6 +77,6 @@ export default defineConfig([
     },
   },
   {
-    ignores: ["db/migrations/**/*", "node_modules/*", "public/*", ".github/*", "dist/*", "**/*.d.ts"],
+    ignores: ["src/db/migrations/**/*", "node_modules/*", "public/*", ".github/*", "dist/*", "**/*.d.ts", "**/*.json", "**/*.sql"],
   },
 ]);
